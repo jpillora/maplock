5c725ac6e22a5b5ee4260c93c8c67ee8e60b14b4
Jaime Pillora <dev@jpillora.com>
2016-04-20 11:29:25 +1000

    swapped to docker's implementation

--- maplock.go
+++ maplock.go
@@ -2,32 +2,99 @@ package maplock
 
 import "sync"
 
-func New() *MapLock {
-	return &MapLock{m: map[string]*sync.Mutex{}}
+import (
+	"errors"
+	"sync/atomic"
+)
+
+// ErrNoSuchLock is returned when the requested lock does not exist
+var ErrNoSuchLock = errors.New("no such lock")
+
+// Maplock provides a locking mechanism based on the passed in reference name
+type Maplock struct {
+	mu    sync.Mutex
+	locks map[string]*lockCtr
 }
 
-type MapLock struct {
-	l sync.Mutex
-	m map[string]*sync.Mutex
+// lockCtr is used by Maplock to represent a lock with a given name.
+type lockCtr struct {
+	mu sync.Mutex
+	// waiters is the number of waiters waiting to acquire the lock
+	// this is int32 instead of uint32 so we can add `-1` in `dec()`
+	waiters int32
 }
 
-func (m *MapLock) Lock(key string) {
-	m.l.Lock()
-	l, ok := m.m[key]
-	if !ok {
-		l = &sync.Mutex{}
-		m.m[key] = l
+// inc increments the number of waiters waiting for the lock
+func (l *lockCtr) inc() {
+	atomic.AddInt32(&l.waiters, 1)
+}
+
+// dec decrements the number of waiters waiting on the lock
+func (l *lockCtr) dec() {
+	atomic.AddInt32(&l.waiters, -1)
+}
+
+// count gets the current number of waiters
+func (l *lockCtr) count() int32 {
+	return atomic.LoadInt32(&l.waiters)
+}
+
+// Lock locks the mutex
+func (l *lockCtr) Lock() {
+	l.mu.Lock()
+}
+
+// Unlock unlocks the mutex
+func (l *lockCtr) Unlock() {
+	l.mu.Unlock()
+}
+
+// New creates a new Maplock
+func New() *Maplock {
+	return &Maplock{
+		locks: make(map[string]*lockCtr),
 	}
-	m.l.Unlock()
-	l.Lock()
 }
 
-func (m *MapLock) Unlock(key string) {
-	m.l.Lock()
-	l, ok := m.m[key]
-	m.l.Unlock()
-	if !ok {
-		return
+// Lock locks a mutex with the given name. If it doesn't exist, one is created
+func (l *Maplock) Lock(name string) {
+	l.mu.Lock()
+	if l.locks == nil {
+		l.locks = make(map[string]*lockCtr)
 	}
-	l.Unlock()
+
+	nameLock, exists := l.locks[name]
+	if !exists {
+		nameLock = &lockCtr{}
+		l.locks[name] = nameLock
+	}
+
+	// increment the nameLock waiters while inside the main mutex
+	// this makes sure that the lock isn't deleted if `Lock` and `Unlock` are called concurrently
+	nameLock.inc()
+	l.mu.Unlock()
+
+	// Lock the nameLock outside the main mutex so we don't block other operations
+	// once locked then we can decrement the number of waiters for this lock
+	nameLock.Lock()
+	nameLock.dec()
+}
+
+// Unlock unlocks the mutex with the given name
+// If the given lock is not being waited on by any other callers, it is deleted
+func (l *Maplock) Unlock(name string) error {
+	l.mu.Lock()
+	nameLock, exists := l.locks[name]
+	if !exists {
+		l.mu.Unlock()
+		return ErrNoSuchLock
+	}
+
+	if nameLock.count() == 0 {
+		delete(l.locks, name)
+	}
+	nameLock.Unlock()
+
+	l.mu.Unlock()
+	return nil
 }
